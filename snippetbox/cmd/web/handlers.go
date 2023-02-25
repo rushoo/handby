e2c105110ac4c91f134d0983d018106b207d93d9
rushoo <603370603@qq.com>
2023-02-25 11:05:24 +0800

    使用httprouter代替默认路由

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"snippetbox/internal/models"
 	"strconv"
@@ -11,11 +12,6 @@ import (
 //通过app的依赖注入，这里的handler就可以获取到数据库连接并使用model{}操作数据库
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	//避免根路径全匹配
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 	snippets, err := app.snippet.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -28,7 +24,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	//从请求中获取id
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.Atoi(params.ByName("id"))
+	//id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -49,12 +47,9 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	//定义接受的请求类型
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", "POST")
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+	w.Write([]byte("Display the form for creating a new snippet..."))
+}
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
 	title := "O snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
@@ -65,5 +60,5 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// 303重定向到页面"/snippet/view?id=id"显示刚刚插入的结果
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- snippetbox/cmd/web/routes.go
+++ snippetbox/cmd/web/routes.go
@@ -1,25 +1,34 @@
 package main
 
 import (
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
+	//使用httprouter
+	router := httprouter.New()
+
+	//自定义空路径请求的响应
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
 
 	//通过内置方法以 "./ui/static"路径创建一个文件服务
 	//文件服务器接受以"/static/"开头的请求.在具体访问前去掉"/static"前缀
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/", http.StripPrefix("/static", fileServer))
+	//router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	//chain := New(app.recoverPanic, app.logRequest, secureHeaders)
 	chain := New()
 	chain.Append(app.recoverPanic)
 	chain.Append(app.logRequest, secureHeaders)
-	return chain.Then(mux)
-	//return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return chain.Then(router)
+	//return app.recoverPanic(app.logRequest(secureHeaders(router)))
 }
